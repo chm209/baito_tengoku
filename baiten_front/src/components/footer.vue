b5da0b1d858aa238f1cced426716e68c68b9af06
chm209 <changmin7537@gmail.com>
2023-05-21 22:39:42 +0900

    메인페이지 작업중
    * 헤더, 푸터, 노티 작업완료
    * 최근 글 작업중

---
+++ baiten_front/src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href="https://chm209.github.io">© 2023 LEE CHANG MIN all right reserved</a>
+</template>
+  
+  <script>
+  export default {
+    name: 'footer'
+  }
+  </script>
+  
+  <style scoped>
+    footer {
+      width: 100%;
+      font-size: 1rem;
+    }
+  </style>
+  
---
+++ baiten_front/src/components/header.vue
@@ -0,0 +1,47 @@
+<template>
+<header>
+  <table>
+    <tr>
+      <td id="header-title"><router-link to="/">BAITEN</router-link></td>
+      <td class="header-link" align="right"><router-link to="login">로그인</router-link></td>
+    </tr>
+  </table>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  font-size: 1rem;
+  padding-top: 60px;
+  padding-bottom: 30px;
+}
+
+header table {
+  width: 100%;
+  height: 100%;
+  border-bottom: 1px solid grey;
+  border-collapse: collapse;
+	margin: 0 auto;
+}
+
+header table tr td {
+  padding-bottom: 20px;
+}
+
+header table tr #header-title {
+  font-size: 2.2rem;
+	font-weight: 800;
+	border: 0;
+}
+
+header table tr .header-link {
+  font-size: 1.2rem;
+}
+</style>
